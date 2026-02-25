0aa7f763b62c8bf1d9348e3eae1a56f4efccca28
zyhwan <zyhwan010410@naver.com>
2026-02-25 09:19:53 +0900

    클론 후 정수 숫자 입력 추가
    클론 후 정수 숫자 입력을 하나 더 추가해 산술연산자를 더 사용해서 추가하였다.

--- main.cpp
+++ main.cpp
@@ -7,13 +7,15 @@ int main()
 	int Number1 = 0;
 	int Number2 = 0;
 
-	cin >> Number1 >> Number2;
+	int Number3 = 0;
 
-	cout << "합: " << Number1 + Number2 << '\n';
-	cout << "차: " << Number1 - Number2 << '\n';
-	cout << "곱: " << Number1 * Number2 << '\n';
-	cout << "몫: " << Number1 / Number2 << '\n';
-	cout << "나머지: " << Number1 % Number2 << '\n';
+	cin >> Number1 >> Number2 >> Number3;
+
+	cout << "합: " << Number1 + Number2 + Number3 << '\n';
+	cout << "차: " << Number1 - Number2 - Number3 << '\n';
+	cout << "곱: " << Number1 * Number2 * Number3 << '\n';
+	cout << "몫: " << Number1 / Number2 / Number3 << '\n';
+	cout << "나머지: " << Number1 % Number2 % Number3 << '\n';
 
 	return 0;
 }
