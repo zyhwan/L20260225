d24656ee84f70e5af05f6a20fc17e0146aa9c44a
zyhwan <zyhwan010410@naver.com>
2026-02-25 16:05:27 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,22 +7,25 @@ int main()
 {
 	int Map[10][10] = {
 	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 1, 1, 1, 1, 0, 1, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 1, 0, 1},
+	{1, 1, 0, 1, 1, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 1, 0, 1, 1, 1, 1},
+	{1, 1, 1, 0, 1, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 1, 0, 1, 1, 0, 1},
+	{1, 1, 1, 0, 1, 0, 1, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 1, 0, 9, 1},
 	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
 	};
 
 	int PlayerX = 1;
 	int PlayerY = 1;
 
+	//GameLoop
+	//Frame
 	for (;;)
 	{
+		//Render
 		for (int Y = 0; Y < 10; ++Y)
 		{
 			for (int X = 0; X < 10; ++X)
@@ -33,19 +36,48 @@ int main()
 				}
 				else if (Map[Y][X] == 1)
 				{
-					cout << '#';
+					cout << "#";
 				}
 				else if (Map[Y][X] == 0) {
 					cout << ' ';
+				}
+
+				if (Map[Y][X] == 9) {
+					cout << 'G';
 				}
 				cout << ' ';
 			}
 			cout << '\n';
 		}
 
+		//Process();
 		int Temp;
 		Temp = _getch();
-		PlayerX++;
+
+		if (Temp == 'w' || Temp == 'W') //w키
+		{
+			if (Map[PlayerY - 1][PlayerX] != 1) PlayerY--;
+		}
+		if (Temp == 's' || Temp == 'S') //s키
+		{
+			if (Map[PlayerY + 1][PlayerX] != 1) PlayerY++;
+		}
+		if (Temp == 'a' || Temp == 'A') //a키
+		{
+			if (Map[PlayerY][PlayerX - 1] != 1) PlayerX--;
+		}
+		if (Temp == 'd' || Temp == 'D') //d키
+		{
+			if (Map[PlayerY][PlayerX + 1] != 1) PlayerX++;
+		}
+		if (Map[PlayerY][PlayerX] == 9)
+		{
+			Map[PlayerY][PlayerX] = '@';
+			cout << "골인~!";
+			break;
+		}
+		system("cls");
 	}
+
 	return 0;
 }
